cd0c0f69a800784ab0870498c54da08db829dd2c
Mashy Green <mashy@me.com>
2020-03-16 09:35:45 +0000

    updating scripts for some stuff

--- scripts/workspace2.sh
+++ scripts/workspace2.sh
@@ -2,6 +2,6 @@
 
 killall -q picom
 i3-msg "workspace 2; append_layout /home/mashy/.i3/workspace-2.json"
-termite & termite & termite & termite & termite &
+st & st & st & st & st &
 while pgrep -u $UID -x picom >/dev/null; do sleep 0.5; done
 picom -b

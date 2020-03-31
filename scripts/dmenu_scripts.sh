e54d00b929a4b54cee27d1e33828157fe0c573b5
Mashy <mashy@me.com>
2020-03-31 09:28:59 +0100

    added scripts, minor changes in i3 and polybar configs

--- scripts/dmenu_scripts.sh
+++ scripts/dmenu_scripts.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 #
-
-choices="UTC connect\nUTC nas\nUTC disconnect\nMopidy"
-
+# list of choices
+choices="UTC connect\nUTC nas\nUTC disconnect\nMopidy\nPicom\nWS2"
+# launch dmenu
 chosen=$(echo -e "$choices" | dmenu -i -n -l 15)
-
+# run the script
 case "$chosen" in
     "UTC connect") [ $(echo -e "Yes\nNo" | dmenu -i -n -l 15 -p "Confirm?") == "Yes" ] && $HOME/.scripts/utc.sh -c ;;
     "UTC nas") [ $(echo -e "Yes\nNo" | dmenu -i -n -l 15 -p "Confirm?") == "Yes" ] && $HOME//.scripts/utc.sh -n ;;
     "UTC disconnect") [ $(echo -e "Yes\nNo" | dmenu -i -n -l 15 -p "Confirm?") == "Yes" ] && $HOME/.scripts/utc.sh -d ;;
-    "Mopidy") $HOME/.scripts/mopidy.sh
+    "Mopidy") $HOME/.scripts/mopidy.sh ;;
+    "Picom") $HOME/.scripts/picom.sh ;;
+    "WS2") $HOME/.scripts/workspace2.sh
 esac

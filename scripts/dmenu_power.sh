41f4ed4c1db419d5992f6f943275246045a04a03
Mashy Green <mashy@me.com>
2020-04-04 22:42:35 +0100

    cleaning up OpenSUSE dotfiles

---
+++ scripts/dmenu_power.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+# list of choices
+choices="Lock\nShutdown\nRestart\nHibernate\nSleep\nLogout"
+# launch dmenu
+chosen=$(echo -e "$choices" | dmenu -i -n -l 15)
+# run the script
+case "$chosen" in
+    "Lock"      ) [ $(echo -e "Yes\nNo" | dmenu -i -n -l 15 -p "Confirm?") == "Yes" ] && $HOME/.scripts/lock.sh;;
+    "Shutdown"  ) [ $(echo -e "Yes\nNo" | dmenu -i -n -l 15 -p "Confirm?") == "Yes" ] && systemctl poweroff;;
+    "Restart"   ) [ $(echo -e "Yes\nNo" | dmenu -i -n -l 15 -p "Confirm?") == "Yes" ] && systemctl reboot ;;
+    "Hibernate" ) [ $(echo -e "Yes\nNo" | dmenu -i -n -l 15 -p "Confirm?") == "Yes" ] && systemctl hibernate ;;
+    "Sleep"     ) [ $(echo -e "Yes\nNo" | dmenu -i -n -l 15 -p "Confirm?") == "Yes" ] && systemctl suspend ;;
+    "Logout"    ) [ $(echo -e "Yes\nNo" | dmenu -i -n -l 15 -p "Confirm?") == "Yes" ] && systemctl restart lightdm.service
+esac

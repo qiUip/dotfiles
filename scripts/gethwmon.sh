c0ad1f1f481d6ff7c859fea9b35bc4e74bebe32d
Mashy <mashy@me.com>
2021-10-11 13:05:20 +0100

    changes mainly to scripts - distro agnostic checkupdates

--- scripts/gethwmon.sh
+++ scripts/gethwmon.sh
@@ -1,7 +1,4 @@
-# Prints an integer value of the temperature value directly from the sysfs path
-# Use `sensors` to find preferred temperature source, then run
-# $ for i in /sys/class/hwmon/hwmon*/temp*_input; do echo "$(<$(dirname $i)/name): $(cat ${i%_*}_label 2>/dev/null || echo $(basename ${i%_*})) $(readlink -f $i)"; done
-# to find path to desired file.
-# The location of the desired file is exported to a env variable to be used by the ctemp script for xmobar.  The selected sensor is "Package id" - change it to the desired sensor you with to use as needed.
+# Creates a enviroment variable for the path of the CPU temperature sensor.  This script should be added to /etc/profile.d to be executed upon startup.
+# The selected sensor is "Package id" - change it to the desired sensor you with to use as needed.
 
 export hwmon_id=$(for i in /sys/class/hwmon/hwmon*/temp*_input; do echo "$(<$(dirname $i)/name): $(cat ${i%_*}_label 2>/dev/null || echo $(basename ${i%_*})) $(readlink -f $i)"; done | awk '/Package id/{print $NF}')
--- scripts/workspace2.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-killall -q picom
-i3-msg "workspace 2; append_layout /home/mashy/.config/i3/workspaces/workspace-2.json"
-st & st & st & st & st
-while pgrep -u $UID -x picom >/dev/null; do sleep 0.5; done
-picom -b
